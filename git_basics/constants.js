e90949d2b91f9a718207c28a4ae0842ea440510d
olegKobilinskiy <olegkobilinskij@gmail.com>
2025-12-23 08:44:36 +0200

    add import constans

---
+++ git_basics/constants.js
@@ -0,0 +1,3 @@
+export const passed = 120;
+export const failed = 23;
+export const flaky = 45;
--- git_basics/newFile.js
+++ git_basics/newFile.js
@@ -1,4 +1,6 @@
 import chalk from 'chalk'
+import {passed, flaky, failed} from "./constants.js";
 
-console.log(chalk.green('Test passed'))
-console.log(chalk.red('Test failed'))
+console.log(chalk.green('Test passed: ', passed));
+console.log(chalk.red('Test failed: ', failed));
+console.log(chalk.yellow('Test flaky: ', flaky));
