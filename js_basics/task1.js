1958301517d7ad8ce858277e2025a47ca957a7a2
olegKobilinskiy <olegkobilinskij@gmail.com>
2026-01-11 13:28:35 +0200

    js basics home work

---
+++ js_basics/task1.js
@@ -0,0 +1,14 @@
+const num = 2;
+console.log(num);
+
+const str = "Hello world";
+console.log(str);
+
+const bool = true;
+console.log(bool);
+
+const nUll = null;
+console.log(nUll);
+
+const undefined = undefined;
+console.log(undefined);
---
+++ js_basics/task4.js
@@ -0,0 +1,15 @@
+const circleRadius = 26;
+const areaOfCircle = Math.PI * Math.pow(circleRadius, 2);
+
+console.log(areaOfCircle);
+
+const length = 12;
+const width = 27;
+const areaOfRectangle = length * width;
+console.log(areaOfRectangle);
+
+const cylinderRadius = 28;
+const cylinderHeight = 7;
+const volumeOfCylinder = (Math.PI * Math.pow(cylinderRadius, 2) * cylinderHeight);
+console.log(volumeOfCylinder.toFixed(2));
+
